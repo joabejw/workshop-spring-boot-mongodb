6b239cdfcd85ff8e3c5820e2e33b6efe5e8a6e3b
joabe <joabe.18@hotmail.com>
2023-01-11 16:49:19 -0300

    Consulta com vários critérios

--- src/main/java/com/joabejw/workshopmongo/resources/PostResource.java
+++ src/main/java/com/joabejw/workshopmongo/resources/PostResource.java
@@ -1,11 +1,10 @@
 package com.joabejw.workshopmongo.resources;
 
-import java.security.Provider.Service;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,19 +22,31 @@ public class PostResource {
 	@Autowired
 	private PostService postrService;
 
-
 	@GetMapping
 	@RequestMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postrService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
 	@RequestMapping(value = "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value= "text", defaultValue = "") String text) {
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = postrService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping
+	@RequestMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postrService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 
