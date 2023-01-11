8ebe128e19444265d713bae79568e0681ea75c72
joabe <joabe.18@hotmail.com>
2023-01-11 16:14:58 -0300

    Consulta simples com @Query

--- src/main/java/com/joabejw/workshopmongo/Repositories/PostRepository.java
+++ src/main/java/com/joabejw/workshopmongo/Repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.joabejw.workshopmongo.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.joabejw.workshopmongo.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContaining(String text);
 }
--- src/main/java/com/joabejw/workshopmongo/resources/PostResource.java
+++ src/main/java/com/joabejw/workshopmongo/resources/PostResource.java
@@ -1,13 +1,19 @@
 package com.joabejw.workshopmongo.resources;
 
+import java.security.Provider.Service;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joabejw.workshopmongo.entities.Post;
+import com.joabejw.workshopmongo.resources.util.URL;
 import com.joabejw.workshopmongo.services.PostService;
 
 @RestController
@@ -23,6 +29,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postrService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value= "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postrService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
